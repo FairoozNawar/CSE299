62f406359415a60ff457baf64a4439f2c916cfc6
FairoozNawar <fairooz.nawar@northsouth.edu>
2022-12-27 02:23:38 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<br>
+
+	  <footer>
+	  	<p style="font-weight: 900;">© Fonimonosha BD 2022  |  <span style="font-weight: 100;"> Designed, Developed & Maintained by <a href="" target="_blank">Fairooz Nawar Adnin</a></span></p>
+	  </footer>
+
+	  
+
+
+
+</body>
+</html>
